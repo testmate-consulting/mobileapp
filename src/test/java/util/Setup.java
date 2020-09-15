5defa9cc2ba77981f0dd8409b3a8d041e67d1949
sandiwise <sandisiwe.consultant@gmail.com>
2020-09-15 14:49:41 +0200

    Initial commit

---
+++ src/test/java/util/Setup.java
@@ -0,0 +1,21 @@
+package util;
+
+import org.junit.BeforeClass;
+
+import java.net.MalformedURLException;
+
+public class Setup {
+
+    private static final String  PACKAGE ="";
+    private static final String ENVIRONMENT = "";
+
+    //protected static AppiumDriver driver;
+    protected static final String APP_ID ="";
+    protected static final String DEVICE ="";
+
+    @BeforeClass
+    public static void Setup()throws MalformedURLException {
+       // Disir
+
+    }
+}
