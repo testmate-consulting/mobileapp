ee7d9ad13a1952d1260a61d4c3c1bb7e755c5b53
sandiwise <sandisiwe.consultant@gmail.com>
2020-09-15 15:21:52 +0200

    Add setup class

--- src/test/java/util/Setup.java
+++ src/test/java/util/Setup.java
@@ -1,21 +1,34 @@
 package util;
 
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
 import org.junit.BeforeClass;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.springframework.cache.annotation.CacheAnnotationParser;
 
 import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class Setup {
 
-    private static final String  PACKAGE ="";
-    private static final String ENVIRONMENT = "";
+    private static final String  PACKAGE ="com.example.android.uamp";
+    //private static final String ENVIRONMENT = "";
 
-    //protected static AppiumDriver driver;
+    protected static AppiumDriver driver;
     protected static final String APP_ID ="";
-    protected static final String DEVICE ="";
+    protected static final String DEVICE = "LCL7N18428001913 ";
 
     @BeforeClass
     public static void Setup()throws MalformedURLException {
-       // Disir
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+        capabilities.setCapability("deviceName",DEVICE);
+        capabilities.setCapability("platformName","Android");
+        capabilities.setCapability("appPackage",PACKAGE);
+        capabilities.setCapability("appActivity",PACKAGE + "ui.MusicPlayerActivity");
+        capabilities.setCapability("noReset",true);
 
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
+        //driver.manage().timeouts().implicitlyWait(ge, TimeUnit.SECONDS);
     }
 }
